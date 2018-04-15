dfa982caf59db37cb7a428802b87849e69ca442c
Eric Qian <eric@enumc.com>
2018-04-15 01:37:48 -0700

    Remove Extraneous Code

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -208,7 +208,7 @@ export const navigation = [
   {
     name: 'View GitHub Project',
     url: 'https://github.com/BananiumLabs/CheeryClass/',
-    icon: 'icon-stack',
+    icon: 'icon-layers',
     class: 'mt-auto',
     variant: 'success'
   }
